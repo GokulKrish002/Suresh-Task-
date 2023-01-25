3658cb5db77f6948f49a88e89c9d7f0613d49d42
Gokul <itsgokul2002@gmail.com>
2023-01-25 22:59:39 +0530

    Add project files.

---
+++ Index.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Suresh.Index" %>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Userlock</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <style>
+        .lights{
+            height: 140px;
+            width:  140px;
+        }
+        .table-area{
+            margin-right: 2%;
+        }
+        .status-area{
+            font-size: 50px;
+        }
+    </style>
+
+</head>
+    <body>
+
+        <div class="container-fluid">
+            <form method="post" runat="server">
+            <div class="row">
+                <div class="col-lg-8 col-md-12 table-area">
+                     
+                    <div class="input area m-5">
+                        <p class="status-area">Search</p>
+                    <div class="d-flex">
+                        <asp:TextBox ID="TextBox1"  runat="server" class="form-control"></asp:TextBox>
+                        <asp:Button ID="Button1" runat="server"  class="btn btn-primary" Text="Button" OnClick="Button1_Click1" />
+                    </div>
+                </div>
+                    <asp:GridView ID="TableView" runat="server">
+                        <FooterStyle CssClass="table" />
+                         </asp:GridView>
+                         
+                </div>
+                <div class="col-lg-3 col-md-12 m-5 ml-5 ">
+                        <h1 class="status-area">Status</h1>
+                        <div id="lightRed" class="lights rounded-circle bg-secondary"></div>
+                        <br>
+                        <div id="lightYellow" class="lights rounded-circle bg-secondary"></div>
+                        <br>
+                        <div id="lightGreen" class="lights rounded-circle bg-secondary"></div>
+                        <br>
+                </div>
+            </div>
+        </form>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="script.js"></script>
+    </body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Suresh
+{
+    public partial class Index : System.Web.UI.Page
+    {
+
+        public void Tbl_refresh()
+        {
+            SqlConnection connect = new SqlConnection("Data Source=GOKULSVICTUS\\SQLEXPRESS;Initial Catalog=suresh_db;Integrated Security=True");
+            connect.Open();
+            SqlCommand getData = new SqlCommand("GetTable", connect);
+            getData.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(getData);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            TableView.DataSource = dt;
+            TableView.DataBind();
+            connect.Close();
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Tbl_refresh();
+        }
+
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            SqlConnection connect = new SqlConnection("Data Source=GOKULSVICTUS\\SQLEXPRESS;Initial Catalog=suresh_db;Integrated Security=True");
+            connect.Open();
+            SqlCommand cmd = new SqlCommand("sp_search", connect);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Email", TextBox1.Text);
+            SqlDataReader readed;
+            readed = cmd.ExecuteReader();
+
+            if(readed.Read())
+            {
+                string status = readed["status"].ToString();
+                if (status == "Online")
+                {
+                    
+                }
+                else if (status == "Busy")
+                {
+
+                }
+                else if (status == "Offline")
+                {
+
+                }
+            }
+            else
+            {
+                Response.Write("What..?");
+            }
+
+
+
+
+            Tbl_refresh();
+        }
+
+   /*         SqlCommand cmd = new SqlCommand("sp_search_Username1", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", txt_name.Text);
+            cmd.ExecuteNonQuery();           
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+            grid1.DataSource = dt;
+            grid1.DataBind();
+*/
+
+    }
+}
